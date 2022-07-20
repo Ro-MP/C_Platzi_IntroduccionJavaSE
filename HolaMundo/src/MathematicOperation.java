f59192e49eab64971c60759bea74403e9d5e4e49
Rodrigo Márquez Picazo <r.mpicazo.97gmail.com>
2022-07-20 07:54:34 -0500

    Se ocupó clase Math

---
+++ HolaMundo/src/MathematicOperation.java
@@ -0,0 +1,24 @@
+public class MathematicOperation {
+    public static void main(String[] args) {
+
+        double x = 2.1;
+        double y = 3;
+
+        // Redondeo hacia arriba
+        System.out.println(Math.ceil(x));
+
+        // Redondeo hacia abajo
+        System.out.println(Math.floor(x));
+
+        // Devuelve numero elevado a otro
+        System.out.println(Math.pow(x, y));
+
+        // Devuelve el numero mayor
+        System.out.println(Math.max(x, y));
+
+        // Devuelve raiz cuadrada
+        System.out.println(Math.sqrt(y));
+
+
+    }
+}
